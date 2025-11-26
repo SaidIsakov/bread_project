121a0608328f4a6069c7d8f8abc56f747b764649
Said <pm5581287@gmail.com>
2025-11-26 22:01:39 +0300

    add categories in project

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from .models import Product
+from .models import Product, Category
 
 
 class IndexView(TemplateView):
@@ -9,4 +9,5 @@ class IndexView(TemplateView):
   def get_context_data(self, **kwargs):
       context = super().get_context_data(**kwargs)
       context["products"] = Product.objects.all()
+      context['categories'] = Category.objects.all()
       return context
