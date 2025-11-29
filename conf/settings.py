7464d6c6960d8837dadb3de52c0efd1b6b2a6815
Said <pm5581287@gmail.com>
2025-11-29 13:31:35 +0300

    changes in footer/about_us and hero

--- conf/settings.py
+++ conf/settings.py
@@ -116,7 +116,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
